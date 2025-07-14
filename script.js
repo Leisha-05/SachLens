492494249da4c92b00a65e21d4c92a304151924f
Leisha Gupta <guptaleisha9@gmail.com>
2025-07-14 23:38:43 +0530

    about panel added

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("scrolled");
   }
 });
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".about-panel", {
+  yPercent: -100, 
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".hero",
+    start: "top top",
+    end: "bottom top",
+    scrub: true,
+    pin: ".hero",
+  }
+});
